55b37e96054e9977c622ca4076d1ed8f5b70c02d
haclesrocha200@gmail.com <71661551+hacles100@users.noreply.github.com>
2023-01-19 11:40:14 -0100

    create routers page

---
+++ cruder/src/routes/Feed.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Feed(){
+
+    return(
+        <div>
+            <h1>Feed Page</h1>
+        </div>
+    )
+}
+
+export default Feed
---
+++ cruder/src/routes/ReadMore.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function ReadMore(){
+
+    return(
+        <div>
+            <h1>ReadMore Page</h1>
+        </div>
+    )
+}
+
+export default ReadMore
