4ab2d15c746c234250576d3c0c9d6914b9921ef2
haclesrocha200@gmail.com <71661551+hacles100@users.noreply.github.com>
2023-01-19 11:48:23 -0100

    Create routers done

--- cruder/src/main.jsx
+++ cruder/src/main.jsx
@@ -1,10 +1,54 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
+
+
+import {  createBrowserRouter,  RouterProvider } from "react-router-dom";
+
+// Import Routes
+import Edit from './routes/Edit';
+import Feed from './routes/Feed';
+import Post from './routes/Post';
+import ReadMore from './routes/ReadMore';
+
+
+// configuracao do roteamento
+
+const router = createBrowserRouter([
+  {
+    element: <App/>,
+    children: [
+
+      // rota feed
+      {
+        path: "/",
+        element: <Feed/>
+      },
+
+      // rota edit
+      {
+        path: "edit",
+        element: <Edit/>
+      },
+
+      {
+        path: "post",
+        element: <Post/>
+      },
+
+      {
+        path: "read",
+        element: <ReadMore/>
+      }
+    ]
+  }
+])
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
